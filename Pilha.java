9e4e880bb5bddd56cbe1fbdeb02ab2a6a05a3333
Felipe Fernandes Gomes <felipeffggomes@hotmail.com>
2021-05-31 15:23:56 -0300

    novo print
    Reformat code
    
    idea
    
    Escritas em ingles para portugues

--- Pilha.java
+++ Pilha.java
@@ -1,55 +1,47 @@
 public class Pilha {
-  private int topo = -1;
-  private int maximo;
-  private char[] dado;
-  public Pilha(int maximo){
-      this.maximo = maximo;
-      this.dado = new char[maximo];
-  }
-  public boolean isEmpty(){
-      return topo == -1;
-  }
-  public boolean isFull(){
-      return topo == maximo - 1;
-  }
+    private int topo = -1;
+    private int maximo;
+    private char[] dado;
 
-  public void empilhar(char a){
-          if (!isFull()){
-                  topo = topo + 1;
-                  dado[topo] =  a;
-          }
-          else{
-          System.out.println("Pilha Cheia");
-          }
+    public Pilha(int maximo) {
+        this.maximo = maximo;
+        this.dado = new char[maximo];
+    }
 
-  }
- public int getTopo() {
-     if (!isEmpty()) {
-         return dado[topo];
-     }
-     else {
-         return -1;
-     }
- }
+    public boolean Vazio() {
+        return topo == -1;
+    }
+
+    public boolean Cheio() {
+        return topo == maximo - 1;
+    }
 
 
+    public void empilhar(char a) {
+        if (!isFull()) {
+            topo = topo + 1;
+            dado[topo] = a;
+        } else {
+            System.out.println("Pilha Cheia");
+        }
+    }
 
+    public int getTopo() {
+        if (!isEmpty()) {
+            return dado[topo];
+        } else {
+            return -1;
+        }
+    }
 
-  public int  desempilhar(){
-          if (!isEmpty()){
-                 int x = getTopo();
-                  topo = topo -1;
-                  return x;
-          }
-          else{
-               System.out.println("Pilha vazia.");
-               return -1;
-          }
-
-
-
-  }
-
-
-
+    public int desempilhar() {
+        if (!isEmpty()) {
+            int x = getTopo();
+            topo = topo - 1;
+            return x;
+        } else {
+            System.out.println("Pilha vazia.");
+            return -1;
+        }
+    }
 }
